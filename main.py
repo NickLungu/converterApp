978d88652574048292420c14efaee532a97532a6
NickLungu <43681819+NickLungu@users.noreply.github.com>
2022-11-22 23:52:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from PIL import Image
 import easygui
+import os
 
 
 def convert4tg(input_file):
-    path = input_file
-    im1 = Image.open(path)
-    width, height = im1.size
+    path_list = input_file
 
-    path_out = ('.'.join(path.split(".")[:-1])) + ".png"
+    for counter, path in enumerate(path_list):
 
-    coef = 512/max(width, height)
+        # working with path
+        if counter == 0:
+            splited_path = path.split("\\")
+            folder = '\\'.join(splited_path[:-1])
+            new_folder = folder + '\\' + "converted"
+        path_out = new_folder + '\\' + str(counter+1) + ".png"
 
-    width_new = width * coef
-    height_new = height * coef
+        # open and work with image
+        im1 = Image.open(path)
+        width, height = im1.size
+        coef = 512/max(width, height)
+        width_new = width * coef
+        height_new = height * coef
+        im1_out = im1.resize((int(width_new), int(height_new)))
 
-    im1_out = im1.resize((int(width_new), int(height_new)))
+        # save image
+        if not os.path.isdir(new_folder):
+            os.makedirs(new_folder)
+        im1_out.save(path_out)
 
-    im1_out.save(path_out)
+    return len(path_list), new_folder
 
-
-if __name__ == '__main__':
-
-    input_file = easygui.fileopenbox(filetypes=["*.img"])
-    convert4tg(input_file)
 
--- window.py
+++ window.py
@@ -1,23 +1,50 @@
 from main import convert4tg
 import easygui
 import tkinter as tk
-
-
+import pyperclip
+import os
 
 class App:
+    path = ""
+    num_files = ""
     def __init__(self):
 
         self.root = tk.Tk()
-        self.root.title('Конвертер изображений')
-        self.root.geometry('500x400')
-        self.btn = tk.Button(self.root, text='Загрузить изображение', command=self.get_file)
-        self.btn.pack(expand=True)
+
+        self.root.title('Image converter')
+        self.root.geometry('600x250')
+        self.root['background'] = '#DAFFEF'
+
+        # self.back_image = tk.PhotoImage(file="bg.png")
+        self.l = tk.Label(self.root, text = "")
+        self.l['background'] = '#DAFFEF'
+        self.l.bind("<Button-1>", self.printKey)
+
+        self.btn = tk.Button(self.root,
+                             font=('calibri', 15),
+                             text='load images',
+                             command=self.get_file
+                             )
+
+        self.btn.pack(expand=1)
+        self.l.pack(padx=0, pady=40)
 
         self.root.mainloop()
 
+
     def get_file(self):
-        input_file = easygui.fileopenbox(filetypes=["*.img"])
-        convert4tg(input_file)
+        input_file = easygui.fileopenbox(default="*.jpg", filetypes=["*.jpeg", "*.jpg"], multiple=True)
+        if not input_file == None:
+            self.num_files, self.path = convert4tg(input_file)
+            wording = "File is ready" if self.num_files == 1 else "Files are ready"
+            self.l.configure(text=wording + ": (click here to open folder): \n\n"+self.path)
+            # msg = "files are ready here: \n\n" + path
+            # tk.messagebox.showinfo("Information", msg)
+
+    def printKey(self,a=0):
+        pyperclip.copy(self.path)
+        os.system(r"explorer.exe " + self.path)
+
 
 
 if __name__ == '__main__':
